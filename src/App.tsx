7db16343179a8fd32ce5536728fa455bef5bed39
imhwarang <imhwarang4@naver.com>
2025-07-30 17:34:17 +0900

    chore: setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Global, ThemeProvider } from "@emotion/react"
 import theme from "./styles/theme"
 import GlobalStyle from "./styles/global"
+import MovieConcept from "./pages/MovieConcept/MovieConcept"
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyle}/>
+      <MovieConcept />
     </ThemeProvider>
   )
 }
