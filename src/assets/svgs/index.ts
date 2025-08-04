466145e51464fc684aa663f97df230bb7513904d
imhwarang <imhwarang4@naver.com>
2025-08-04 21:43:19 +0900

    feat: MessageCard 컴포넌트 구현

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,3 @@
 export { default as Copy } from "./Copy";
 export { default as IcArrowGray0716 } from "./IcArrowGray0716";
+export { default as IcDeleteGray0412 } from "./IcDeleteGray0412";
---
+++ src/components/common/MessageCard/MessageCard.style.ts
@@ -0,0 +1,27 @@
+import type { Theme } from "@emotion/react";
+import { css } from "@emotion/react";
+import { flexGenerator } from "../../../styles/generator";
+
+export const cardWrapperStyle = (theme: Theme) => css`
+    ${flexGenerator("column", "flex-start", "flex-start")}
+    width: 100%;
+    gap: 1.4rem;
+    border-radius: 4px;
+    background-color: ${theme.colors.white};
+    padding: 1.6rem 1.7rem;
+`;
+
+export const cardHeaderStyle = css`
+    ${flexGenerator("row", "space-between")}
+    width: 100%;
+`;
+
+export const nicknameStyle = (theme: Theme) => css`
+    ${theme.fonts.mb2_body02_14_m}
+    color: ${theme.colors.black};
+`;
+
+export const cardTextStyle = (theme: Theme) => css`
+    ${theme.fonts.mc2_caption02_12_re}
+    color: ${theme.colors.gray05};
+`;
